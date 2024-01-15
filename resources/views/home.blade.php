12ac380c11d74ac5999181d7f5f5337f6d1d7b2e
serena-peschiaroli <serena.peschiaroli@gmail.com>
2024-01-15 15:44:24 +0100

    Add: cards with movies implemented

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,24 @@
 @section('content')
 
 <section>
-    <h1>test</h1>
+    <div class="container">
+        <div class="row row-cols-5 d-flex gy-4">
+            @foreach ($movies as $movies)
+                <div class="col d-flex align-items-stretch">
+                    <div class="card">
+                        <img src= "{{ $movies->image}}" class="card-img-top" alt="Movie image">
+                        <div class="card-body">
+                            <h5 class="card-title"> {{ $movies->original_title}} </h5>
+                            <p class="card-text"> Nationality: {{$movies->nationality }}</p>
+                            <p class="card-text"> Release: {{$movies->date }}</p>
+                        </div>
+                    </div>
+                </div>
+
+                
+            @endforeach
+        </div>
+    </div>
 
 
 </section>
